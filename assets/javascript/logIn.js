f79b224928b2b32863d6bdb875cf0c5c2b16b3e0
abdul wahid hashoory <whashoory@gmail.com>
2019-04-29 20:38:40 -0500

    fixed the logIn page

--- assets/javascript/logIn.js
+++ assets/javascript/logIn.js
@@ -1,40 +1,36 @@
+var config = {
+    apiKey: "AIzaSyDR_r33Wa1FGkjlSph81ZV53WqTrdoHcqI",
+    authDomain: "project1-21d42.firebaseapp.com",
+    databaseURL: "https://project1-21d42.firebaseio.com",
+    projectId: "project1-21d42",
+    storageBucket: "project1-21d42.appspot.com",
+    messagingSenderId: "718469816684"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+console.log(database);
+
 var attempt = 2; // Variable to count number of attempts.
             $("#logIn").on("click", function (event) {
                 event.preventDefault();
 
                 var email = $("#email").val().trim();
                 var password = $("#password").val().trim();
-                var valid = false;
-                var arrayOfEmail = ["Abdul@gmail.com", "Daneil@gmail.com"];
-                var arrayOfpassword = ["1234", "5678"];
-                console.log(email, password, arrayOfEmail, arrayOfpassword);
-                for (var i = 0; i < arrayOfEmail.length; i++) {
-                    if ((email == arrayOfEmail[i]) && (password == arrayOfpassword[i])) {
-                        console.log("its working");
-                        valid = true;
-                        break;
+                var user = null;
+                database.ref().orderByChild("email").equalTo(email).once("value", function(snapshot){
+                    console.log(snapshot.val());
+                    var foundUsers = snapshot.val();
+                    for (var key in foundUsers) {
+                        console.log(foundUsers[key]);
+                        if (foundUsers[key].password == password) {
+                            user = foundUsers[key];
+                            window.location = "userprofile.html";
+                        }
                     }
-                }
-                if (valid) {
-                    console.log("log in was succesfull");
-                    window.location = "userprofile.html";
-                    return false;
-                }
-                var again = "tries";
-                if (attempt == 1) {
-                    again = "try";
-                }
-                if (attempt >= 1) {
-                    alert("wrong password or email")
-                    attempt--;
-                } else {
-                    alert("Incorrect password or email");
-                    email = "You are now Blocked";
-                    password = "";
-                    $("#email").disabled = true;
-                    $("#password").disabled = true;
-                    return false;
+                    console.log("LOGIN", user)
 
-                }
+                })
 
             });
