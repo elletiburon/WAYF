2e15728039607c4210b754a3b6ca70bef627d997
elletiburon <elleharrer@gmail.com>
2019-05-01 18:32:16 -0500

    chat function preliminary

--- assets/javascript/match.js
+++ assets/javascript/match.js
@@ -94,3 +94,37 @@ function callback(results, status) {
   
   
 }
+
+
+var config = {
+  apiKey: "AIzaSyDR_r33Wa1FGkjlSph81ZV53WqTrdoHcqI",
+  authDomain: "project1-21d42.firebaseapp.com",
+  databaseURL: "https://project1-21d42.firebaseio.com",
+  projectId: "project1-21d42",
+  storageBucket: "project1-21d42.appspot.com",
+  messagingSenderId: "718469816684"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+var user1Email = "bob@bob.com"
+var user2Email = "steve@steve.com"
+
+var refPath = "chat-" + user1Email.replace(".", "_") + "-" + user2Email.replace(".", "_");
+
+database.ref(refPath).on("child_added", function(snapshot){
+  console.log(snapshot.val())
+
+  $("<div>").text(snapshot.val()).appendTo("#messages")
+
+})
+
+$("#send-message").on("click", function(){
+ 
+  var message = $("#message").val()
+  database.ref(refPath).push(message)
+
+  $("#message").val("")
+})
