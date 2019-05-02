cb10027f4f292fcedfcb18c689d624751c54b681
Daniel <daniel.ryuichi.wyatt@gmail.com>
2019-05-01 21:21:00 -0500

    shows other person's info on match page

--- assets/javascript/match.js
+++ assets/javascript/match.js
@@ -12,7 +12,7 @@ firebase.initializeApp(config);
 var database = firebase.database();
 
 var map;
-var service; 
+var service;
 var infowindow;
 var zipcode = sessionStorage.getItem("zipcode");
 var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + zipcode + "&key=AIzaSyBMrJ8Jc38iT-3kb62eF0lhsFvJ69dwaPY";
@@ -26,58 +26,58 @@ function initialize() {
   });
   infoWindow = new google.maps.InfoWindow();
   if (navigator.geolocation) {
-    console.log('******',navigator.geolocation);
+    console.log('******', navigator.geolocation);
     var local = navigator.geolocation.getCurrentPosition(success, block);
     console.log('******LOCAL', local);
-    
 
-  } 
 
-  function block(){
-         
-      $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(response) {
-        console.log("response", response);
-        console.log("lat&long", response.results[0].geometry.location);
-        var userPos = response.results[0].geometry.location;
-        var request = {
-          location: userPos,
-          radius: '500',
-          type: ['restaurant']
-        };
-        service = new google.maps.places.PlacesService(map);
-        service.nearbySearch(request, callback);
-        infoWindow.setPosition(userPos);
-        map.setCenter(userPos);
-        
-      });
-      
-    };
+  }
 
-  function success(position){
+  function block() {
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      console.log("response", response);
+      console.log("lat&long", response.results[0].geometry.location);
+      var userPos = response.results[0].geometry.location;
+      var request = {
+        location: userPos,
+        radius: '500',
+        type: ['restaurant']
+      };
+      service = new google.maps.places.PlacesService(map);
+      service.nearbySearch(request, callback);
+      infoWindow.setPosition(userPos);
+      map.setCenter(userPos);
+
+    });
+
+  };
+
+  function success(position) {
 
     console.log('*******allow')
     var pos = {
       lat: position.coords.latitude,
       lng: position.coords.longitude
     };
-  
+
     var request = {
       location: pos,
       radius: '500',
       type: ['restaurant']
     };
-  
+
     service = new google.maps.places.PlacesService(map);
     service.nearbySearch(request, callback);
     infoWindow.setPosition(pos);
     map.setCenter(pos);
-    
+
   }
-  
-  
+
+
 }
 
 function callback(results, status) {
@@ -87,41 +87,49 @@ function callback(results, status) {
       var place = results[i];
       createMarker(results[i], ninfoWindow);
     }
-    
+
   }
   function createMarker(place, ninfoWindow) {
     console.log(place);
-    console.log("this is ninfoWindow",ninfoWindow)
+    console.log("this is ninfoWindow", ninfoWindow)
     var marker = new google.maps.Marker({
       map: map,
       position: place.geometry.location
     });
-  
+
     google.maps.event.addListener(marker, 'click', function (event) {
-      console.log(event);      
+      console.log(event);
       ninfoWindow.setContent(place.name);
       ninfoWindow.open(map, this);
       map.setCenter(place.geometry.location);
     });
   }
-  
-  
+
+
 }
 var email = sessionStorage.getItem("email");
 var match = sessionStorage.getItem("matchEmail");
-    database.ref().orderByChild("email").equalTo(email).once("value", function(snapshot){
-        var firstName1 = $("<div>");
-        var age1 = $("<div>");
-        var bio1 = $("<div>");
-        firstName1.text("Name: " + sessionStorage.getItem("firstName"));
-        age1.text("Age: " + sessionStorage.getItem("birthday"));
-        bio1.text("About: " + sessionStorage.getItem("about"));
-        $("#info1").append(firstName1, age1, bio1);
+database.ref().orderByChild("email").equalTo(email).once("value", function (snapshot) {
+  var firstName = $("<div>");
+  var firstAge = $("<div>");
+  var firstBio = $("<div>");
+  firstName.text("Name: " + sessionStorage.getItem("firstName"));
+  firstAge.text("Age: " + sessionStorage.getItem("birthday"));
+  firstBio.text("About: " + sessionStorage.getItem("about"));
+  $("#info1").append(firstName, firstAge, firstBio);
+});
 
-        //add session ref
+database.ref().orderByChild("email").equalTo(match).once("value", function (snapshot) {
+  var foundMatch = snapshot.val();
+  var secondName = $("<div>");
+  var secondAge = $("<div>");
+  var secondBio = $("<div>");
+  for (var key in foundMatch) {
+    secondName.text("Name: " + foundMatch[key].firstName);
+    secondAge.text("Age: " + foundMatch[key].birthday);
+    secondBio.text("About: " + foundMatch[key].about);
+    $("#info2").append(secondName, secondAge, secondBio);
 
+  }
 
-        
-        
-
-    });
+});
