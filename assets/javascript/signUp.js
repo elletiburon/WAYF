d7cbc5f2af0ed9205342f9f023ed24e78ce984d0
abdul wahid hashoory <whashoory@gmail.com>
2019-05-03 15:36:02 -0500

    upload user image to firebase storage

--- assets/javascript/signUp.js
+++ assets/javascript/signUp.js
@@ -1,11 +1,3 @@
-$("#needEmail").hide();
-$("#emailExists").hide();
-$("#needPassword").hide();
-$("#needFirstName").hide();
-$("#needLastName").hide();
-$("#needZipcode").hide();
-
-
 var config = {
     apiKey: "AIzaSyDR_r33Wa1FGkjlSph81ZV53WqTrdoHcqI",
     authDomain: "project1-21d42.firebaseapp.com",
@@ -34,12 +26,6 @@ var about = "";
 
 $("#submit").on("click", function (event) {
     event.preventDefault();
-    $("#needEmail").hide();
-    $("#emailExists").hide();
-    $("#needPassword").hide();
-    $("#needFirstName").hide();
-    $("#needLastName").hide();
-    $("#needZipcode").hide();
 
     // YOUR TASK!!!
     // Code in the logic for storing and retrieving the most recent user.
@@ -60,24 +46,12 @@ $("#submit").on("click", function (event) {
         var foundUsers = snapshot.val();
         if (foundUsers) {
             userExist = true;
-            $("#emailExists").show();
-        } else {
-            $("#needEmail").show();
         }
-        if (password == "") {
-            $("#needPassword").show();
-        }
-        if (firstName == "") {
-            $("#needFirstName").show();
-        }
-        if (lastName == "") {
-            $("#needLastName").show();
-        }
-        if (zipcode == 0 || zipcode.toString().length < 5) {
-            $("#needZipcode").show();
-        } else if (!userExist && password && firstName && lastName && zipcode) {
-            console.log("LOGIN ashjdgash", userExist);
+        console.log("userfound", userExist);
 
+        if (!userExist) {
+            console.log("LOGIN ashjdgash", userExist);
+    
             // Code for the push
             database.ref().push({
                 email,
@@ -89,11 +63,11 @@ $("#submit").on("click", function (event) {
                 birthday,
                 zipcode,
                 about
-
-
-
+    
+    
+    
                 // Log everything that's coming out of snapshot
-
+    
                 // full list of items to the well
             }, function (errorObject) {
                 console.log("Errors handled: " + errorObject.code);
@@ -101,8 +75,8 @@ $("#submit").on("click", function (event) {
             //after signUp it will take you to user profile page
             window.location = "userprofile.html";
         }
-        else {
-
+        else{
+            
         }
     });
 
@@ -117,22 +91,6 @@ $("#submit").on("click", function (event) {
     sessionStorage.setItem("zipcode", zipcode);
     sessionStorage.setItem("dateDay", dateDay);
     sessionStorage.setItem("about", about);
-    // By default display the content from sessionStorage
-
-    // Create a root reference
-    // var database = firebase.storage().ref();
-
-    // Create a reference to 'mountains.jpg'
-    var mountainsRef = database.child('mountains.jpg');
-
-    // Create a reference to 'images/mountains.jpg'
-    var mountainImagesRef = database.child('images/mountains.jpg');
-
-    // While the file names are the same, the references point to different files
-    mountainsRef.name === mountainImagesRef.name            // true
-    mountainsRef.fullPath === mountainImagesRef.fullPath    // false
-
-
+  // By default display the content from sessionStorage
 
 });
-
